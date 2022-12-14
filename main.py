07d9799020a9cd3dfdc2d3b4f4cbfd7165db7489
kimspractice <tpghksdldia@naver.com>
2022-12-14 17:05:32 +0900

    selenium install & test

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
+chrome_options = Options()
+chrome_options.add_experimental_option("detach",True)
+
+browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+
+browser.get("https://google.com")
