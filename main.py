f07388d1cdecd4eb9c4db05b62f09436cc5000ff
kimspractice <tpghksdldia@naver.com>
2022-12-15 16:02:45 +0900

    taking screenshot

--- main.py
+++ main.py
@@ -8,17 +8,19 @@ from webdriver_manager.chrome import ChromeDriverManager
 chrome_options = Options()
 chrome_options.add_experimental_option("detach",True)
 
+KEYWORD = "buy domain"
+
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 browser.get("https://google.com")
 
+
 search_bar = browser.find_element(By.CLASS_NAME,"gLFyf")
 
-search_bar.send_keys("hello!")
+search_bar.send_keys(KEYWORD)
 search_bar.send_keys(Keys.ENTER)
 
-search_results = browser.find_elements(By.CLASS_NAME,"g")
+search_results = browser.find_elements(By.CLASS_NAME,"MjjYud")
 
-for search_result in search_results:
-    title = search_result.find_element(By.TAG_NAME,"h3")
-    if title:
-        print(title.text)
+for index, search_result in enumerate(search_results):
+    class_name = search_result.get_attribute("class")
+    capture = search_result.screenshot(f"screenshots/{index}.{KEYWORD}.png")
